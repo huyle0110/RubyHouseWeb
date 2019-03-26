766d5b354805a219dc484d04d15d699e98b21c66
huy <lehuy.paul@gmail.com>
2019-03-26 23:55:07 +0700

    reset gitignore

--- RubyHouseWeb/Scripts/common.js
+++ RubyHouseWeb/Scripts/common.js
@@ -1,1 +1,10 @@
-﻿
+﻿var dateOption = {
+    autoclose: true,
+    format: 'mm/dd/yyyy',
+    language: 'vn',
+    todayHighlight: true,
+
+};
+$(document).ready(function () {
+    $('.FullDateOption').datepicker('set', dateOption);
+});
