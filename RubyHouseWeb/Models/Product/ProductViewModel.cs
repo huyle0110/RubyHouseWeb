49a370be746e03988870a4ef6fd8cc37b9eb5dc7
huyln <huyln@amaris.com>
2019-03-29 17:08:26 +0700

    fix bug Product

---
+++ RubyHouseWeb/Models/Product/ProductViewModel.cs
@@ -0,0 +1,12 @@
+﻿using EntityFrameWorkModule.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RubyHouseWeb.Models
+{
+    public class ProductViewModel : Product
+    {
+    }
+}
